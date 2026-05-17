5e5c6d843e35f773eae3f525b8a9b4016b208241
Fred <dagmawigezahw@gamil.com>
2026-05-17 10:11:24 -0700

    test admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,9 @@
 export const dynamic = "force-dynamic";
-import AuthGuard from "@/components/AuthGuard";
-import ProductForm from "@/components/ProductForm";
 
 export default function AdminPage() {
   return (
-    <AuthGuard>
-      <div className="min-h-screen p-10 bg-gray-100">
-        <h1 className="text-4xl font-bold mb-8">
-          Admin Dashboard
-        </h1>
-
-        <ProductForm />
-      </div>
-    </AuthGuard>
+    <div className="min-h-screen flex items-center justify-center text-3xl font-bold">
+      ADMIN PAGE WORKS
+    </div>
   );
 }
